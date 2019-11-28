8131707f7b6f8d5699dbd228a951206995a049e7
Egidijus Gegeckas <egidijus.gegeckas@gmail.com>
2019-11-28 13:58:41 +0100

    updated SAMPLE.local.js

--- config/SAMPLE.local.js
+++ config/SAMPLE.local.js
@@ -2,20 +2,6 @@ module.exports = {
   environment: 'development',
   server: {
     port: 3000,
-    uploads: {
-      uploadsDir: 'public/uploads',
-      urlPath: '/uploads',
-      allowedTypes: [
-        'image/png',
-        'image/jpg',
-        'image/jpeg',
-        'image/gif',
-        'video/webm',
-        'video/mp4',
-        'video/x-matroska',
-        'application/pdf',
-      ],
-    },
   },
   client: {
     locale: 'en-GB',
